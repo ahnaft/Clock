6ebaeb376c49c1c3c3445a7e09ebfeb9e2b75323
ahnaft <ahnaft2019@gmail.com>
2022-06-18 13:49:02 -0500

    (Bug Fix)

--- Alarm/alerm.js
+++ Alarm/alerm.js
@@ -1,6 +1,23 @@
 const audio = new Audio('mixkit-retro-game-emergency-alarm-1000.wav');
 audio.loop = true;
 
+function rqnoti(){
+    if(Notification.permission === "granted"){;
+    }
+    else if (Notification.permission !== "denied" ) {
+    Notification.requestPermission();}
+    
+    else if(Notification.permission === "denied") {
+        alert("Please Enable Notification")
+        Notification.requestPermission();
+    }
+    
+    
+}
+
+
+rqnoti()
+
 let alarmTime = null;
 let alarmTimeout = null;
 
@@ -10,16 +27,74 @@ let alarmTimeout = null;
    
  }
 
+ function pushnoti(){
+    (async () => {
+       // create and show the notification
+       const showNotification = () => {
+           // create a new notification
+           const notification = new Notification('ALARM!', {
+               body: 'Tap To Dismiss',
+               icon: 'alarm_FILL0_wght400_GRAD0_opsz48.png'
+           });
+   
+           // close the notification after 10 seconds
+           setTimeout(() => {
+               notification.close();
+           }, 10 * 1000);
+   
+           // navigate to a URL when clicked
+           notification.addEventListener('click', () => {
+   
+               clearAlarm();
+           });
+       }
+   
+       // show an error message
+       const showError = () => {
+           const error = document.querySelector('.error');
+           error.style.display = 'block';
+           error.textContent = 'You blocked the notifications';
+       }
+   
+       // check notification permission
+       let granted = false;
+   
+       if (Notification.permission === 'granted') {
+           granted = true;
+       } else if (Notification.permission !== 'denied') {
+           let permission = await Notification.requestPermission();
+           granted = permission === 'granted' ? true : false;
+       }
+   
+       // show notification or error
+       granted ? showNotification() : showError();
+   
+   })();
+    }
+
+
+
+
+
+
+
 
  function setAlarm() {
     if(alarmTime) {
         const current = new Date();
         const timeToAlarm = new Date(alarmTime);
 
+    
+        
         if (timeToAlarm > current) {
             const timeout = timeToAlarm.getTime() - current.getTime();
-            alarmTimeout = setTimeout(() => audio.play(), timeout);
+            alarmTimeout = setTimeout(() => audio.play(), timeout, );
+            alarmTimeout = setTimeout(() => pushnoti(), timeout,);
+                
+
             alert('Alarm set');
+
+           
         }
     }
 }
@@ -38,11 +113,6 @@ function clearAlarm() {
 
 
 
-
-
-
-
-
 var currentTime = new Date().getHours();
 if (document.body) {
     if (7 <= currentTime && currentTime < 20) {
@@ -52,3 +122,6 @@ if (document.body) {
         document.body.background = "nighttime.jpg";
     }
 }
+//notification
+
+
